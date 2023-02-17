4ad3d00d2a3d0a1261da1dd6da9d9689fca914e0
Kseniia Shirokova <ksirokova85@gmail.com>
2023-02-17 14:41:13 +0300

    первая версия проекта

--- src/components/ingredient-details/ingredient-details.js
+++ src/components/ingredient-details/ingredient-details.js
@@ -15,25 +15,25 @@ import IngredientsDetailsStyle from './ingredients-details.module.css'
 export default function IngredientDetails({ ingredients }) {
     return (
         <section className={IngredientsDetailsStyle.section}>
-            <img src={ingredients.image}></img>
-            <p >{ingredients.name}</p>
-            <p>{ingredients.name}</p>
-            <ul>
-            <li>
-                <p>Ккалории, ккал</p>
-                <p>{ingredients.calories}</p>
+            <img src={ingredients.image_large}></img>
+            <p className="text text_type_main-medium mt-4 mb-8">{ingredients.name}</p>
+            
+            <ul className={IngredientsDetailsStyle.ul}>
+            <li className={IngredientsDetailsStyle.list}>
+                <p className="text text_type_main-default text_color_inactive mb-2 mr-5">Ккалории, ккал</p>
+                <p className="text text_type_digits-default text_color_inactive">{ingredients.calories}</p>
             </li>
-            <li>
-                <p>Белки, г</p>
-                <p>{ingredients.fat}</p>
+            <li className={IngredientsDetailsStyle.list}>
+                <p className="text text_type_main-default text_color_inactive mb-2 mr-5">Белки, г</p>
+                <p className="text text_type_digits-default text_color_inactive">{ingredients.fat}</p>
             </li>
-            <li>
-                <p>Жиры. г</p>
-                <p>{ingredients.proteins}</p>
+            <li className={IngredientsDetailsStyle.list}>
+                <p className="text text_type_main-default text_color_inactive mb-2 mr-5">Жиры. г</p>
+                <p className="text text_type_digits-default text_color_inactive">{ingredients.proteins}</p>
             </li>
-            <li>
-                <p>Углеводы, г</p>
-                <p>{ingredients.carbohydrates}</p>
+            <li className={IngredientsDetailsStyle.list}>
+                <p className="text text_type_main-default text_color_inactive mb-2">Углеводы, г</p>
+                <p className="text text_type_digits-default text_color_inactive">{ingredients.carbohydrates}</p>
             </li>
             </ul>
 
--- src/components/modal-overlay/modal-overlay.js
+++ src/components/modal-overlay/modal-overlay.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import ModalOverlayStyle from './modal-overlay.module.css'
-import Modal from '../modal/modal';
-import OrderDetails from '../order-details/order-details'
-import IngredientDetails from '../ingredient-details/ingredient-details';
-import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 
-export default function ModalOverlay({ toClose}) {
-    // React.useEffect(()=>{
-    //     const handleEsc = (event) => {
-    //         event.key === 'Escape' && onClose()
-    //     }
-    //     document.addEventListener('keypress', handleEsc);
-
-    //     return () =>{
-    //         document.removeEventListener('keypress', handleEsc)
-    //     }
-    // }, [onClose()])
+export default function ModalOverlay({ toClose, children}) {
+   
     return (
         <>
             <section className={ModalOverlayStyle.overlay} onClick={toClose}>
-                
+                {children}
                
             </section>
 
