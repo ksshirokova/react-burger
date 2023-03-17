3a8fa45f8043922716e168e97cfbc27531b9b1dd
Kseniia Shirokova <ksirokova85@gmail.com>
2023-03-17 18:46:21 +0300

    добавлен анонимус

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -83,7 +83,7 @@ function App() {
         <Route path='/forgot-password' element={<ForgotPassword />} /> 
         <Route path='/reset-password' element={<ResetPassword />} />
         <Route path='/profile' element={
-          <ProtectedRoute>
+          <ProtectedRoute anonymous={false}>
             <ProfilePage />
            </ProtectedRoute>
         } /> 
--- src/components/protected-route.js/protected-route.js
+++ src/components/protected-route.js/protected-route.js
@@ -4,11 +4,11 @@ import { Navigate, useNavigate, useLocation } from "react-router-dom";
 
 
 
-export default function ProtectedRoute({ children }) {
+export default function ProtectedRoute({ children, anonymous }) {
     const isAuth = useSelector(state => state.routeStore.isAuth)
     const isAuthChecked = useSelector(state => state.routeStore.isAuthChecked)
     const user = useSelector(state => state.routeStore.user)
-    const isLoggedIn = !!useSelector(state => state.routeStore.user)
+    const isLoggedIn = !!useSelector(state => state.routeStore.user.name)
     const loading = useSelector(state => state.routeStore.loading)
     const isLogged = useSelector(state => state.routeStore.isLogged)
     const navigate = useNavigate()
@@ -19,12 +19,12 @@ export default function ProtectedRoute({ children }) {
     // }
     console.log(isLogged)
     
-    if (isLogged) {
+    if (isLoggedIn && isAuthChecked && anonymous)  {
         navigate('/profile', {replace: true})
     }
 
 
-    if (!isLogged) {
+    if (!isLoggedIn && !anonymous) {
         navigate('/login', {replace: true})
     }
 
